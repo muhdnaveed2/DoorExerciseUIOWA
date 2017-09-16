a254075eb4257b94f72ee331337f9cb94db0d0c9
Muhammad Naveed <muhd.naveed2@gmail.com>
2017-09-15 21:13:34 -0400

    Pushing project
    Door passing project

---
+++ DoorExerciseUIOWA/Controllers/DoorController.cs
@@ -0,0 +1,36 @@
+﻿using DoorExerciseUIOWA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoorExerciseUIOWA.Controllers
+{
+    public class DoorController : Controller
+    {
+        // GET: Door
+        public ActionResult Index()
+        {
+            List<DoorModel> doorList = new List<DoorModel>(100);
+
+            //creating 100 doors
+            for (int count = 1; count <= 100; count++)
+            {
+                DoorModel door = new DoorModel(count);
+                doorList.Add(door);
+            }
+
+            // Passing through doors 100 times
+            for (int pass = 1; pass <= 100; pass++)
+            {
+                for (int i = pass; i < doorList.Capacity; i += pass)
+                { 
+                   doorList[i-1].State = doorList[i-1].State == State.Open ? State.Close : State.Open;
+                }
+            }
+            return View("Door", doorList);
+        }
+
+    }
+}
---
+++ DoorExerciseUIOWA/Models/DoorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoorExerciseUIOWA.Models
+{
+    public enum State
+    {
+        Open,
+        Close
+    }
+    public class DoorModel
+    {
+        public DoorModel(int ID)
+        {
+            this.ID = ID;
+            State = State.Close;
+        }
+      public int ID { get; set; }
+      public State State { get; set; }
+    }
+}
