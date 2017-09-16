026f5db877295ea682de389edb4a8eb152de7eb7
Muhammad Naveed <muhd.naveed2@gmail.com>
2017-09-15 21:58:54 -0400

    creating constant variables

--- DoorExerciseUIOWA/Controllers/DoorController.cs
+++ DoorExerciseUIOWA/Controllers/DoorController.cs
@@ -9,20 +9,23 @@ namespace DoorExerciseUIOWA.Controllers
 {
     public class DoorController : Controller
     {
+        const int _DOOR = 100;
+        const int _PASS = 3;
+
         // GET: Door
         public ActionResult Index()
         {
-            List<DoorModel> doorList = new List<DoorModel>(100);
+            List<DoorModel> doorList = new List<DoorModel>(_DOOR);
 
             //creating 100 doors
-            for (int count = 1; count <= 100; count++)
+            for (int count = 1; count <= _DOOR; count++)
             {
                 DoorModel door = new DoorModel(count);
                 doorList.Add(door);
             }
 
             // Passing through doors 100 times
-            for (int pass = 1; pass <= 100; pass++)
+            for (int pass = 1; pass <= _PASS; pass++)
             {
                 for (int i = pass; i < doorList.Capacity; i += pass)
                 { 
